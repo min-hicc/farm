953e0677a3536f10bb32ccffe1da90493301f641
Sharon Moon <jsmoon883@gmail.com>
2023-01-24 17:29:52 -0600

    ass

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour{
+    public CollectableType type;
+    //player walks into collectable
+    //add collectable to player
+    //delete collectable form the cene
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        Player player=collision.GetComponent<Player>();
+
+        if(player){
+            player.inventory.Add(type);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
+
+public enum CollectableType{
+    NONE, COIN
+}
